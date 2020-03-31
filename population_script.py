dd2c2367309d8871bf33b4b5e3f76b9b461a2dc9
Emily Quimby <9906376q@student.gla.ac.uk>
2020-03-31 12:22:33 +0100

    changed name of gitignore.txt to .gitignore

---
+++ population_script.py
@@ -0,0 +1,12 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'nowandthen_project.settings')
+
+import django
+django.setup()
+from nowandthen.models import Comment, Picture
+
+
+# No population script was created for this project, as it was not applicable to this project, given that
+# any population script would need to include images, as all comments, likes and tags need to go against an
+# image.
