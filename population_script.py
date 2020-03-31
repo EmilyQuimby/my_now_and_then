13a39d3be84d117bb286e851408b7ec18ec22978
Emily Quimby <9906376q@student.gla.ac.uk>
2020-03-31 20:14:32 +0100

    updated population script

--- population_script.py
+++ population_script.py
@@ -1,50 +1,104 @@
 import os
 
+from django.contrib.auth.models import User
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mynowandthen.settings')
 
 import django
 
 django.setup()
-from nowandthen.models import Picture, Comment
+from nowandthen.models import Picture, Comment, UserProfile
 
 
 def populate():
+    banana_user = [
+        {'username': 'Banana',
+         'email': 'Banana@cat.com',
+         'password': 'Banana1234'},
+    ]
+    johann_user = [
+        {'username': 'Banana',
+         'email': 'Banana@cat.com',
+         'password': 'Banana1234'},
+    ]
+    wmffre_user = [
+        {'username': 'Wmffre',
+         'email': 'Wmffre@cat.com',
+         'password': 'Wmffre1234'},
+    ]
+
     maryhill_comments = [
-        {'body': 'wowo I love this photo - reminds me of when I used to live there!'},
-        {'body': 'I love Maryhill - its such a pretty part of Glasgow lol'},
-        {'body': 'gonnae no dae that lol', 'user_id': 3}]
+        {'user': banana_user,
+         'body': 'wowo I love this photo - reminds me of when I used to live there!'},
+        {'user': johann_user,
+         'body': 'I love Maryhill - its such a pretty part of Glasgow lol'},
+        {'user': wmffre_user,
+         'body': 'gonnae no dae that lol', 'user_id': 3}],
 
     fireworks_comments = [
-        {'body': 'amazing fireworks - thanks for sharing :)'},
-        {'body': 'love fireworks, love this, love YOU!'},
-        {'body': 'whoop!'}]
+        {'user': banana_user,
+         'body': 'amazing fireworks - thanks for sharing :)'},
+        {'user': johann_user,
+         'body': 'love fireworks, love this, love YOU!'},
+        {'user': wmffre_user,
+         'body': 'whoop!'}]
 
     cityscape_comments = [
-        {'body': 'more pics like this one please!!'},
-        {'body': 'what a sucky picture hahahaaaa'},
-        {'body': 'great - love it!'}]
+        {'user': banana_user,
+         'body': 'more pics like this one please!!'},
+        {'user': johann_user,
+         'body': 'what a sucky picture hahahaaaa'},
+        {'user': wmffre_user,
+         'body': 'great - love it!'}]
 
-    pics = {'media/shared_pics/View-from-kitchen-window-of-Maryhill-tenements.1970.jpg':
-                {'comments': maryhill_comments, 'title': 'Maryhill Laundry', 'description':
-                    'view from my back door when I lived here in the 70s', 'tag_one': 'Maryhill', 'tag_two': 'Laundry',
-                 'era': '1970s', 'likes': 64},
-            'media/shared_pics/fireworks.jpg': {'comments': fireworks_comments, 'title': 'Glasgow Fireworks',
-                                                'description': 'Fireworks at Glasgow Green',
-                                                'tag_one': 'Glasgow', 'tag_two': 'Fireworks',
-                                                'era': '2010s', 'likes': 32},
-            'media/shared_pics/glasgow_cityscape_copy.jpg': {'comments': cityscape_comments, 'title': 'Glasgow Cityscape', 'descrpition':
-                'View over Glasgow', 'tag_one': 'Cityscape', 'tag_two': 'Glasgow',
-                'era': '1990s',  'likes': 16}}
+    pics = {'Maryhill': {'comments': maryhill_comments,
+                         'image': 'shared_pics/View-from-kitchen-window-of-Maryhill-tenements.1970.jpg',
+                         'title': 'Maryhill Laundry',
+                         'description': 'back view',
+                         'tag_one': 'Maryhill',
+                         'tag_two': 'Laundry',
+                         'era': '1970s',
+                         'likes': 64},
+            'Fireworks': {'comments': fireworks_comments,
+                          'image': 'shared_pics/fireworks.jpg',
+                          'title': 'Glasgow Fireworks',
+                          'description': 'Fireworks at Glasgow Green',
+                          'tag_one': 'Glasgow',
+                          'tag_two': 'Fireworks',
+                          'era': '2010s',
+                          'likes': 32},
+            'Cityscape': {'comments': cityscape_comments,
+                          'image': 'shared_pics/glasgow_cityscape_copy.jpg',
+                          'title': 'Glasgow Cityscape',
+                          'descrpition': 'View over Glasgow',
+                          'tag_one': 'Cityscape',
+                          'tag_two': 'Glasgow',
+                          'era': '1990s',
+                          'likes': 16}}
 
     for pic, pic_data in pics.items():
-        p = add_picture(pic, pic_data['title'], pic_data['description'], pic_data['tag_one'], pic_data['tag_two'],
-                        pic_data['era'], pic_data['likes'])
+        p = add_picture(pic,
+                        pic_data['image'],
+                        pic_data['title'],
+                        pic_data['description'],
+                        pic_data['tag_one'],
+                        pic_data['tag_two'],
+                        pic_data['era'],
+                        pic_data['likes'])
         for c in pic_data['comments']:
-            add_comment(p, c['body'])
+            add_comment(p, c['user'], c['body'])
 
     for p in Picture.objects.all():
         for c in Comment.objects.filter(picture=p):
             print(f' - {p}: {c}')
+
+
+def add_user(name, email, password):
+    u = User.objects.get_or_create(username=name, first_name='Test', last_name='User', email=email)[0]
+    u.set_password(password)
+    up = UserProfile.objects.get_or_create(user=u)[0]
+    u.save()
+    return up
 
 
 def add_picture(image, title, description, tag_one, tag_two, era, likes=0):
@@ -58,9 +112,10 @@ def add_picture(image, title, description, tag_one, tag_two, era, likes=0):
     return p
 
 
-def add_comment(image, body):
-    c = Comment.objects.get_or_create(body=body)[0]
-    c.image = image
+def add_comment(user, body):
+    u = User.objects.get(username=user)
+    up = UserProfile.objects.get(user=u)
+    c = Comment.objects.get_or_create(user=up, body=body)[0]
     c.save()
     return c
 
